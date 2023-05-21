1a550ada7e8f5e3a70e559cad189110a39fbab91
Mario Ochoa Tovar <mario8ato@hotmail.com>
2023-05-20 21:50:37 -0600

    firebase config auth

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   KeyboardAvoidingView,
   Platform,
@@ -10,11 +10,26 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const App = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  const onAuthStateChanged = user => {
+    console.log('user???', user);
+    setUser(user);
+    if (initializing) setInitializing(false);
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
   const onPasswordChange = (e: string) => {
     setPassword(e);
@@ -24,12 +39,55 @@ const App = () => {
     setEmail(e);
   };
 
+  const createUser = (emailInput: string, passwordInput: string) => {
+    setLoading(true);
+    auth()
+      .createUserWithEmailAndPassword(emailInput, passwordInput)
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+        setLoading(false);
+        setError('Ocurrio un error');
+        console.error(error);
+      });
+  };
+
+  const signOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
   const submit = () => {
     if (email === '' || password === '') {
       setError('Llene los campos');
       return;
     }
+    createUser(email, password);
   };
+
+  if (initializing) return null;
+
+  if (user) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hola {user.email}</Text>
+        <TouchableOpacity style={styles.button} onPress={signOut}>
+          <Text style={{color: 'white', fontSize: 20, textAlign: 'center'}}>
+            Cerrar sesion
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
   return (
     <SafeAreaView style={styles.sectionContainer}>
@@ -53,8 +111,8 @@ const App = () => {
           />
           {error && <Text style={{color: 'red'}}>{error}</Text>}
 
-          <TouchableOpacity style={styles.button} onPress={() => submit()}>
-            <Text style={{color: 'white', fontSize: 20, textAlign: 'center'}}>
+          <TouchableOpacity style={styles.button} onPress={submit}>
+            <Text style={{color: 'white', fontSize: 20, textAlign: 'center'}} disabled={loading}>
               Registrarse
             </Text>
           </TouchableOpacity>
